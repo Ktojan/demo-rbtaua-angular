56aa3cbec4288cf3156f65b47bfe9be995522c86
Andrey <karpovich.andrey@gmail.com>
2024-11-09 12:45:37 +0200

    implementation of error handling when fetching vacancies

--- src/app/vacancies.service.ts
+++ src/app/vacancies.service.ts
@@ -1,27 +1,27 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
-
-type ResponseData = {
-  documents: Array<any>;
-};
+import { catchError, map, Observable, of, tap } from 'rxjs';
+import { DEFAULT_VACANCIES } from './model/default-data';
 
 const proxy = 'https://thingproxy.freeboard.io/fetch/'; //to handle CORS when developing
 
-
 @Injectable({ providedIn: 'root'}) export class VacanciesService {
 
-  // private apiUrl = 'https://api.rabota.ua/vacancy/search'
   private apiUrl = 'https://dracula.robota.ua/'
 
   constructor(private http: HttpClient) {}
   
-
-  public fetchVacancies(body = {}, params = {q: 'getPublishedVacanciesList'}): Observable<any[]> {    
-    return this.http.post<any>(this.apiUrl, body, { params })
+  public fetchVacancies(body = {}, params = {q: 'getPublishedVacanciesList'}): Observable<any[]> {  
+    return this.http.post<any>(proxy + this.apiUrl, body, { params })
       .pipe(
         tap(data => console.log('Get vacancies result: ', data)),
-        map(res => res.data.publishedVacancies.items)
+        map(res => res.data.publishedVacancies.items),
+        catchError(err => this.handleFetchVacanciesError(err))
       )
   }  
+
+  handleFetchVacanciesError(err: HttpErrorResponse): Observable<any[]> {
+    console.error('Помилка при завантаженні вакансій ', err);
+    return of(DEFAULT_VACANCIES);
+}
 }
